cfddebf498702312dbf4482e2b0f1c92f2a88f4e
slaytanic87 <le872002@yahoo.de>
2017-08-23 22:30:48 +0200

    add TextTure pack & Gunshots

--- core/src/org/megamangdx/game/utils/B2WorldCreator.java
+++ core/src/org/megamangdx/game/utils/B2WorldCreator.java
@@ -36,7 +36,7 @@ public class B2WorldCreator {
         FixtureDef fdef = new FixtureDef();
         Body body;
 
-        for(MapObject object : map.getLayers().get(name).getObjects().getByType(RectangleMapObject.class)) {
+        for (MapObject object : map.getLayers().get(name).getObjects().getByType(RectangleMapObject.class)) {
             Rectangle rect = ((RectangleMapObject) object).getRectangle();
 
             bdef.type = BodyDef.BodyType.StaticBody;
